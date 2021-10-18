7f89dec1609a488c75cb1373867bdf8f4a444bbc
Izzeldin Addarda <38206617+zelsaddr@users.noreply.github.com>
2021-10-18 17:44:27 +0700

    Added responsive table

--- admin/list_photo.php
+++ admin/list_photo.php
@@ -4,35 +4,37 @@ if(!defined("TwibbonKu")){
     exit();
 }
 ?>
-    <table id="dataTables" class="table table-responsive">
-        <thead class="thead-dark">
-            <tr>
-                <th>ID</th>
-                <th>Preview</th>
-                <th>File Name</th>
-                <th>File Size</th>
-                <th>Date</th>
-                <th>Delete</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php
-            $data = $twibbon->show_photo();
-            foreach($data as $row){
-        ?>
-            <tr>
-                <td><?php echo $row['id']; ?></td>
-                <td><img id="img_preview" src=".<?php echo $row['file_location']; ?>" width="100px" height="100px"></td>
-                <td><?php echo $row['file_name']; ?></td>
-                <td><?php echo $twibbon->filesize_formatted(".".$row['file_location']); ?></td>
-                <td><?php echo $row['date']; ?></td>
-                <td><a href="./?do=delete&opt=photo&id=<?php echo $row['id']; ?>" class="btn btn-danger btn-xs">Delete</a></td>
-            </tr>
-        <?php
-            }
-        ?>
-        </tbody>
-    </table>
+    <div class="table-responsive">
+        <table id="dataTables" class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Preview</th>
+                    <th>File Name</th>
+                    <th>File Size</th>
+                    <th>Date</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                $data = $twibbon->show_photo();
+                foreach($data as $row){
+            ?>
+                <tr>
+                    <td><?php echo $row['id']; ?></td>
+                    <td><img id="img_preview" src=".<?php echo $row['file_location']; ?>" width="100px" height="100px"></td>
+                    <td><?php echo $row['file_name']; ?></td>
+                    <td><?php echo $twibbon->filesize_formatted(".".$row['file_location']); ?></td>
+                    <td><?php echo $row['date']; ?></td>
+                    <td><a href="./?do=delete&opt=photo&id=<?php echo $row['id']; ?>" class="btn btn-danger btn-xs">Delete</a></td>
+                </tr>
+            <?php
+                }
+            ?>
+            </tbody>
+        </table>
+    </div>
     <script>
         $("img").hover(
             function(){$(this).animate({width: "250px", height:"250px"}, 1000);},        
